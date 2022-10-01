2a6909d5c472def487db216195805efdedee2cad
wardvisual <wardvisual@gmail.com>
2022-10-01 19:05:08 +0000

    chore(x86_64/boot): implement stack and check multiboot

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -3,6 +3,32 @@ global start
 section .text
 bits 32
 start:
+    mov esp, stack_stop
+
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
     ; print `OK`
     mov dword [0xb8000], 0x2f4b2f4f
     hlt 
+
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+.no_multiboot:
+    mov al, "M"
+    jmp error
+
+error:
+    ; print "ERR, :X " where X is the error code
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte [0xb800a], al
+    hlt
+section .bss
+stack_bottom:
+    resb 4096 * 4
+stack_stop:
