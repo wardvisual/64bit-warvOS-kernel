9bb2c982390929d54ea15fb35e154f398eac6b65
wardvisual <wardvisual@gmail.com>
2022-10-01 19:19:29 +0000

    chore(impl/x86_64/boot): check cpu long mode

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -38,6 +38,23 @@ check_cpuid:
 .no_cupid:
     mov al, "C"
     jmp error
+
+
+check_long_mode:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb .no_long_mode
+
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz .no_long_mode
+
+    ret
+.no_long_mode:
+    mov al, "L"
+    jmp error
     
 error:
     ; print "ERR, :X " where X is the error code
